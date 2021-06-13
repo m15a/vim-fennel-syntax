b19b0affe4f4b81b137042f5c240ab4aa074620d
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2021-06-13 19:24:28 +0900

    Install fennel from luarocks

--- shell.nix
+++ shell.nix
@@ -18,13 +18,10 @@ let
     packageOverrides = self: super: {
       fennel = self.buildLuarocksPackage rec {
         pname = "fennel";
-        version = "0.9.2";
-        knownRockspec = "${src}/rockspecs/fennel-scm-2.rockspec";
-        src = pkgs.fetchFromGitHub {
-          owner = "bakpakin";
-          repo = "Fennel";
-          rev = version;
-          sha256 = "1kpm3lzxzwkhxm4ghpbx8iw0ni7gb73y68lsc3ll2rcx0fwv9303";
+        version = "0.9.2-1";
+        src = pkgs.fetchurl {
+          url    = "mirror://luarocks/${pname}-${version}.src.rock";
+          sha256 = "1ki1cm33f2vlgyargs1p30ixppvvzl0fznnyhwvr6x70g91damd9";
         };
         disabled = (self.luaOlder "5.1");
         propagatedBuildInputs = [ lua ];
