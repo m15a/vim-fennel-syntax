75945c8c8114de87f3c2ad93845f42b15d634658
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-02-23 14:39:41 +0900

    tools(nix): use flake-fennel-tools

--- flake.nix
+++ flake.nix
@@ -2,24 +2,29 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    fennel-tools = {
+      url = "github:m15a/flake-fennel-tools";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, fennel-tools, ... }:
     let
       overlay = final: prev:
         {
-          lua = prev.lua.override {
-            packageOverrides = self: super: {
-              gumbo = self.buildLuarocksPackage rec {
-                pname = "gumbo";
-                version = "0.5-1";
-                src = final.fetchurl {
-                  url = "mirror://luarocks/${pname}-${version}.src.rock";
-                  sha256 = "0p36d63bjckn36yv9jly08igqjkx7xicq4q479f69njiaxlhag6f";
+          fennel-luajit = prev.fennel-luajit.override {
+            lua = prev.luajit.override {
+              packageOverrides = self: super: {
+                gumbo = self.buildLuarocksPackage rec {
+                  pname = "gumbo";
+                  version = "0.5-1";
+                  src = final.fetchurl {
+                    url = "mirror://luarocks/${pname}-${version}.src.rock";
+                    sha256 = "0p36d63bjckn36yv9jly08igqjkx7xicq4q479f69njiaxlhag6f";
+                  };
+                  sourceRoot = "${pname}-${version}/lua-${pname}-0.5";
+                  propagatedBuildInputs = [ final.luajit ];
                 };
-                sourceRoot = "${pname}-${version}/lua-${pname}-0.5";
-                disabled = self.luaOlder "5.1";
-                propagatedBuildInputs = [ final.lua ];
               };
             };
           };
@@ -29,19 +34,26 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ overlay ];
+          overlays = [
+            fennel-tools.overlays.default
+            overlay
+          ];
         };
       in
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            vim-vint
-          ] ++ (with lua.pkgs; [
-            fennel
-            readline
-            luasocket
-            gumbo
-          ]);
-        };
+        devShells.default =
+          let
+            fennel = pkgs.fennel-luajit;
+          in
+          pkgs.mkShell {
+            buildInputs = with pkgs; [
+              vim-vint
+              fennel
+            ] ++ (with fennel.lua.pkgs; [
+              readline
+              luasec
+              gumbo
+            ]);
+          };
       });
 }
