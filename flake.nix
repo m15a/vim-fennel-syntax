97a61dc949e15354b42bc1e1a09ebe63e0b6645d
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-11-01 21:00:44 +0900

    chore: format Nix code

--- flake.nix
+++ flake.nix
@@ -8,29 +8,36 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, fennel-tools, ... }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      fennel-tools,
+      ...
+    }:
     let
-      overlay = final: prev:
-        {
-          fennel-luajit = prev.fennel-luajit.override {
-            lua = prev.luajit.override {
-              packageOverrides = self: super: {
-                gumbo = self.buildLuarocksPackage rec {
-                  pname = "gumbo";
-                  version = "0.5-1";
-                  src = final.fetchurl {
-                    url = "mirror://luarocks/${pname}-${version}.src.rock";
-                    sha256 = "0p36d63bjckn36yv9jly08igqjkx7xicq4q479f69njiaxlhag6f";
-                  };
-                  sourceRoot = "${pname}-${version}/lua-${pname}-0.5";
-                  propagatedBuildInputs = [ final.luajit ];
+      overlay = final: prev: {
+        fennel-luajit = prev.fennel-luajit.override {
+          lua = prev.luajit.override {
+            packageOverrides = self: super: {
+              gumbo = self.buildLuarocksPackage rec {
+                pname = "gumbo";
+                version = "0.5-1";
+                src = final.fetchurl {
+                  url = "mirror://luarocks/${pname}-${version}.src.rock";
+                  sha256 = "0p36d63bjckn36yv9jly08igqjkx7xicq4q479f69njiaxlhag6f";
                 };
+                sourceRoot = "${pname}-${version}/lua-${pname}-0.5";
+                propagatedBuildInputs = [ final.luajit ];
               };
             };
           };
         };
+      };
     in
-    flake-utils.lib.eachDefaultSystem (system:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
@@ -46,16 +53,21 @@
             fennel = pkgs.fennel-luajit;
           in
           pkgs.mkShell {
-            buildInputs = with pkgs; [
-              vim-vint
-              fennel
-            ] ++ (with fennel.lua.pkgs; [
-              readline
-              luasec
-              gumbo
-            ]);
+            buildInputs =
+              with pkgs;
+              [
+                nixfmt-rfc-style
+                vim-vint
+                fennel
+              ]
+              ++ (with fennel.lua.pkgs; [
+                readline
+                luasec
+                gumbo
+              ]);
             FENNEL_PATH = "./_fnl/?.fnl;./_fnl/?/init.fnl";
             FENNEL_MACRO_PATH = "./_fnl/?.fnl;./_fnl/?/init.fnl";
           };
-      });
+      }
+    );
 }
