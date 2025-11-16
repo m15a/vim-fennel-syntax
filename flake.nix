d481c4c45af1cc3126962c1dad4810a073da0967
NACAMURA Mitsuhiro <m15@m15a.dev>
2025-11-16 17:03:45 +0900

    chore: reduce deps in the flake

--- flake.nix
+++ flake.nix
@@ -1,74 +1,66 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    fennel-tools = {
-      url = "github:m15a/flake-fennel-tools";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      fennel-tools,
-      ...
-    }:
+    { nixpkgs, ... }:
     let
+      defaultSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forDefaultSystems =
+        f:
+        nixpkgs.lib.genAttrs defaultSystems (
+          system:
+          f (
+            import nixpkgs {
+              inherit system;
+              overlays = [ overlay ];
+            }
+          )
+        );
       overlay = final: prev: {
-        fennel-luajit = prev.fennel-luajit.override {
-          lua = prev.luajit.override {
-            packageOverrides = self: super: {
-              gumbo = self.buildLuarocksPackage rec {
-                pname = "gumbo";
-                version = "0.5-1";
-                src = final.fetchurl {
-                  url = "mirror://luarocks/${pname}-${version}.src.rock";
-                  sha256 = "0p36d63bjckn36yv9jly08igqjkx7xicq4q479f69njiaxlhag6f";
-                };
-                sourceRoot = "${pname}-${version}/lua-${pname}-0.5";
-                propagatedBuildInputs = [ final.luajit ];
+        luajit = prev.luajit.override {
+          packageOverrides = self: super: {
+            gumbo = self.buildLuarocksPackage rec {
+              pname = "gumbo";
+              version = "0.5-1";
+              src = final.fetchurl {
+                url = "mirror://luarocks/${pname}-${version}.src.rock";
+                sha256 = "0p36d63bjckn36yv9jly08igqjkx7xicq4q479f69njiaxlhag6f";
               };
+              sourceRoot = "${pname}-${version}/lua-${pname}-0.5";
+              propagatedBuildInputs = [ final.luajit ];
             };
           };
         };
       };
     in
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            fennel-tools.overlays.default
-            overlay
+    {
+      devShells = forDefaultSystems (pkgs: rec {
+        formatting = pkgs.mkShell {
+          packages = with pkgs; [
+            vim-vint
           ];
         };
-      in
-      {
-        devShells.default =
-          let
-            fennel = pkgs.fennel-lua5_3;
-          in
-          pkgs.mkShell {
-            buildInputs =
-              with pkgs;
-              [
-                nixfmt-rfc-style
-                vim-vint
+        default = pkgs.mkShell {
+          inputsFrom = [ formatting ];
+          packages = with pkgs; [
+            (luajit.withPackages (
+              ps: with ps; [
                 fennel
-                fennel-ls-unstable
-              ]
-              ++ (with fennel.lua.pkgs; [
                 readline
                 luasec
                 gumbo
-              ]);
-            FENNEL_PATH = "./?.fnl;./?/init.fnl;./tools/?.fnl;./tools/?/init.fnl";
-            FENNEL_MACRO_PATH = "./?.fnl;./?/init-macros.fnl;./tools/?.fnl;./tools/?/init-macros.fnl";
-          };
-      }
-    );
+              ]
+            ))
+            fennel-ls
+          ];
+          FENNEL_PATH = "./?.fnl;./?/init.fnl;./tools/?.fnl;./tools/?/init.fnl";
+          FENNEL_MACRO_PATH = "./?.fnl;./?/init-macros.fnl;./tools/?.fnl;./tools/?/init-macros.fnl";
+        };
+      });
+    };
 }
